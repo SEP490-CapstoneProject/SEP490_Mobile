c313d2a0dee1fa2c5e76afbb283b0bb9a03af25e
Quyen-Peter <quyentrinh040@gmail.com>
2026-01-10 14:36:37 +0700

    notification

--- app/(tabs)/chat/notification.tsx
+++ app/(tabs)/chat/notification.tsx
@@ -7,7 +7,7 @@ import { useEffect, useState } from "react";
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 
 export default function Notification() {
-    const router = useRouter();
+  const router = useRouter();
   const { id } = useLocalSearchParams<{ id: string }>();
   const [detail, setDetail] = useState<NotificationDetail>();
 
@@ -17,8 +17,8 @@ export default function Notification() {
   return (
     <View style={styles.container}>
       {/** header */}
-      <View>
-        <View>
+      <View style={styles.headerContainer}>
+        <View style={styles.headerLeft}>
           <Pressable onPress={() => router.back()}>
             <Image
               source={require("../../../assets/myApp/arrow.png")}
@@ -27,7 +27,12 @@ export default function Notification() {
           </Pressable>
           <Text style={styles.name}>SkillSnap</Text>
         </View>
+        <Image
+          source={require("../../../assets/myApp/list.png")}
+          style={styles.headerIcon}
+        />
       </View>
+      {/** body */}
     </View>
   );
 }
@@ -35,14 +40,28 @@ export default function Notification() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: 30,
+    backgroundColor: "#EFF6FF",
+  },
+  headerContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
+    paddingVertical: 15,
+    borderBottomColor: "#E2E8F0",
+    borderBottomWidth: 1,
+    backgroundColor: "#FFFFFF",
+    paddingTop: 45,
+  },
+  headerLeft: {
+    flexDirection: "row",
+    gap: 15,
   },
   headerIcon: {
     width: 23,
     height: 23,
   },
-  name:{
-    fontSize: 17,
+  name: {
+    fontSize: 18,
     fontWeight: "bold",
-  }
+  },
 });
